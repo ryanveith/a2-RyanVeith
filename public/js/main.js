267cb016b37274d22988fab4751abffbf4f0699f
ryanveith <ryanleeveith@gmail.com>
2026-08-31 14:56:09 -0400

    quick save before I just redo, since I don't see a good not to

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,11 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
   
-  const playername = document.querySelector( '#yourname' ),
+  const mode = document.querySelector( '#option' ),
         username = document.querySelector( '#username' ),
         score = document.querySelector( '#highscore' ),
         today = new Date().toISOString().slice(0, 10), 
-        json = { yourname: playername.value, username: username.value, highscore: score.value, date: today},
+        json = { option: mode.value, username: username.value, highscore: score.value, date: today},
         body = JSON.stringify( json )
 
   const response = await fetch( '/submit', {
@@ -24,7 +24,17 @@ const submit = async function( event ) {
   const data  = JSON.parse(text)
   let dataToDisplay = ""
   for (let i = 0; i < data.length; i++) {
-      dataToDisplay += `<li> Ranking: ${data[i].ranking} Player: ${data[i].abbriviation} Score: ${data[i].highscore} on ${data[i].date} by ${data[i].yourname}</li>`
+    let ending = "th"
+      if (i == 0) {
+        ending = "st"
+      }
+      else if (i == 1) {
+        ending = "nd"
+      }
+      else if (i == 2) {
+        ending = "rd"
+      }
+      dataToDisplay += `<li> ${data[i].ranking}${ending} Player: ${data[i].abbriviation} Score: ${data[i].highscore} on ${data[i].date} by ${data[i].username}</li>`
   }
   // Overwrite the displayed scoretable with the updated version after it returns
   document.getElementById('scoretable').innerHTML = dataToDisplay
--- server.js
+++ server.js
@@ -42,33 +42,97 @@ const handlePost = function( request, response ) {
 
   request.on( 'end', function() {
     const data = JSON.parse( dataString )
-    //console.log( data )
     // ... do something with the data here!!!
     
     // 3 Letter abbriviation for your username
     // Derived automatically
-    data.abbriviation = data.username.slice(0,3)
+    data.abbriviation = data.username.slice(0,3).toUpperCase()
+
+    // Make sure score is valid and format it
+    if (Number(data.highscore) === NaN) {
+      data.highscore = 0
+    }
+    if (data.highscore > 999) {
+      data.highscore = 999
+    }
+    data.highscore = String(data.highscore).padStart(3, "0")
+
 
     // Second derived attribute is current ranking compared to other scores
     // This requires going though data and not only comparing scores but also updating other rankings
-    let placedscore = false
+    let placedscore = null
+    let indexToRemove = null
+    console.log("Start")
     for (let index = 0; index < serverdata.length; index++) {
-      if (placedscore) {
-        serverdata[index].ranking = index + 1
-      }
-      else {
-        if (serverdata[index].highscore <= data.highscore) {
+      if (data.option !== "Delete Score") {
+        // Check if this is the spot to add the "new" score
+        if (Number(serverdata[index].highscore) <= Number(data.highscore)) {
           data.ranking = index + 1
-          serverdata.splice(index, 0, data)
-          placedscore = true
+          placedscore = index
+          console.log("placed score", index)
         }
       }
+      if (serverdata[index].username === data.username) {
+        console.log("HI")
+        //This score either needs to be updated, removed, or is end of search
+        // Add/Remove/Modify Score
+        if (data.option === "Modify Score" || data.option === "Delete Score") {
+          // Remove the current score
+          indexToRemove = index
+        }
+        if (data.option === "Add Score") {
+          if (placedscore !== null) {
+            // We already added a score remove the worse one, otherwise don't bother adding a non-highscore
+            indexToRemove = index
+            console.log("plan remove")
+            break
+          }
+          console.log(index, indexToRemove)
+          placedscore = "Skip This"
+          break
+        }
+      }
+      if (placedscore !== null && indexToRemove !== null) {
+        // No other scores will change so don't bother finishing
+        console.log("DONE")
+        break
+      }
     }
-    if (placedscore == false) {
+    if (placedscore === null && (data.option !== "Delete Score")) {
+      // Append new lowscore to bottom of the list
       data.ranking = serverdata.length + 1
       serverdata.push(data)
+      placedscore = serverdata.length
+    }
+    else {
+      // Append new scores after we are done iterating though the array
+      if (placedscore === "Skip This") {
+        // Special case we did not place the score only pretended to since it was not a highscore
+        placedscore = null
+        console.log("WTH")
+      }
+      else {
+        serverdata.splice(placedscore, 0, data)
+      }
+    }
+    // Remove stuff from the array now that we are done iterating though its
+    if (indexToRemove !== null) {
+      // if this is after we added a index go 1 index further
+      if (placedscore !== null && placedscore <= indexToRemove) {
+        indexToRemove++
+        console.log("hello")
+      }
+      serverdata.splice(indexToRemove, 1)
+      console.log("REMOVING INDEX", indexToRemove)
     }
     
+    // Iterate though the array agian and update rankings
+    // The efficiency of only going though the array once was not worth the
+    // potential errors of doing this during editing the array
+    for (let index = 0; index < serverdata.length; index++) {
+      serverdata[index].ranking = index + 1
+    }
+
     // console.log("returning data")
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
 
